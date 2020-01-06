e1e02fd1927aaff25e0295adffa6c642802427fa
whhhh1500 <cc15009080204@163.com>
2020-01-06 16:46:39 +0800

    删除部分代码

--- opc/src/main/java/com/cc1500/controller/MyStartRunner.java
+++ opc/src/main/java/com/cc1500/controller/MyStartRunner.java
@@ -10,8 +10,6 @@ import org.openscada.opc.lib.da.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.*;
@@ -65,7 +63,7 @@ public class MyStartRunner implements CommandLineRunner {
 
 
 
-    @RequestMapping(value = "/starcallback", method = RequestMethod.GET)
+
     private String callback() {
         String callbackflag = "失败";
         String host = getEntryValue( CONFIG_HOST );
--- opc/src/main/java/com/cc1500/controller/OpcItemInfoController.java
+++ opc/src/main/java/com/cc1500/controller/OpcItemInfoController.java
@@ -14,7 +14,6 @@ import java.util.List;
  * @since 2019-11-12 08:52:21
  */
 @RestController
-//@RequestMapping("/OpcItemInfo")
 public class OpcItemInfoController {
     /** * 服务对象  */
     @Resource
@@ -31,76 +30,4 @@ public class OpcItemInfoController {
         return "index";
     }
 
-
-    @GetMapping("/selectOne")
-    public String selectOne(Integer id) {
-        OpcItemInfo obj= this.opcItemInfoService.queryById(id);
-        if(obj!=null){
-            System.out.println(obj.toString());
-            return ResultVo.success("查询成功", 0,  obj);
-        }
-        return ResultVo.error("查询失败", 500, null);
-    }
-    
-    /**
-     * @Description 通过实体作为筛选条件查询
-     * @param opcItemInfo 实例对象
-     * @return 对象列表
-     */
-    @GetMapping("/queryAll")
-    public String queryAll(OpcItemInfo opcItemInfo) {
-        List<OpcItemInfo> list=this.opcItemInfoService.queryAll(opcItemInfo);
-        if(list!=null){
-            return ResultVo.success("查询list成功", 0,  list);
-        }
-        return ResultVo.error("查询list失败", 500, null);
-     }
-     /**
-     * @Description insert
-     * @param opcItemInfo 
-     * @return 单条数据
-     */
-    @PostMapping("/insert")
-    public String insert(OpcItemInfo opcItemInfo) {
-        OpcItemInfo obj= this.opcItemInfoService.insert(opcItemInfo);
-        if(obj!=null){
-            return ResultVo.success("添加成功", 0,  obj);
-        }
-        return ResultVo.error("添加失败", 500, null);
-  }
-    @PostMapping("/insertOr")
-    public String insertOr(OpcItemInfo opcItemInfo) {
-        int  obj=0;
-                obj= this.opcItemInfoService.insertOr(opcItemInfo);
-        if(obj>0){
-            return ResultVo.success("添加成功", 0,  null);
-        }
-        return ResultVo.error("添加失败", 500, null);
-    }
-     /**
-     * @Description  update
-     * @param opcItemInfo
-     * @return 单条数据
-     */
-    @PutMapping("/update")
-    public String update(OpcItemInfo opcItemInfo) {
-        OpcItemInfo obj= this.opcItemInfoService.update(opcItemInfo);
-        if(obj!=null){
-            return ResultVo.success("更新成功", 0,  obj);
-        }
-        return ResultVo.error("更新失败", 500, null);
-    }
-      /**
-     * @Description delete
-     * @param id
-     * @return 单条数据
-     */
-    @PostMapping("/deleteById")
-    public String DeleteMapping(Integer id) {
-        boolean obj=this.opcItemInfoService.deleteById(id);
-        if(obj){
-            return ResultVo.success("删除成功", 0,obj);
-        }
-        return ResultVo.error("删除失败", 500, null);
-   }  
 }
